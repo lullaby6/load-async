0072c9ebd309d1456e1f39c1d8a3ab816ace8a9b
LucianoBrumer <lucianobrumer5@gmail.com>
2024-09-06 12:06:42 -0300

    loadAsync

--- load-async.cdn.js
+++ load-async.cdn.js
@@ -1,4 +1,6 @@
 function loadJS(src, onLoad = null, parent = document.head) {
+    if (Array.isArray(src)) return src.map(s => loadJS(s, onLoad, parent));
+
     if (parent.querySelector(`script[src="${src}"]`)) return null;
 
     const script = document.createElement('script');
@@ -6,13 +8,18 @@ function loadJS(src, onLoad = null, parent = document.head) {
     script.src = src;
 
     if (typeof onLoad === 'function') script.onload = onLoad;
-    script.onerror = () => console.error(`Failed to load script: ${src}`);
+    script.onerror = () => {
+        console.error(`Failed to load script: ${src}`);
+        script.remove();
+    }
 
     parent.appendChild(script);
     return script;
 }
 
 function loadCSS(src, onLoad = null, parent = document.head) {
+    if (Array.isArray(src)) return src.map(s => loadCSS(s, onLoad, parent));
+
     if (parent.querySelector(`link[href="${src}"]`)) return null;
 
     const link  = document.createElement('link');
@@ -22,7 +29,10 @@ function loadCSS(src, onLoad = null, parent = document.head) {
     link.media = 'all';
 
     if (typeof onLoad === 'function') link.onload = onLoad;
-    link.onerror = () => console.error(`Failed to load stylesheet: ${src}`);
+    link.onerror = () => {
+        console.error(`Failed to load stylesheet: ${src}`);
+        link.remove();
+    }
 
     parent.appendChild(link);
     return link;
@@ -34,6 +44,7 @@ function loadAsyncJS(src, onLoad = null, parent = document.head) {
             if (typeof onLoad === 'function') onLoad(event);
             resolve(script)
         }, parent);
+
         script.onerror = () => reject(new Error(`Failed to load script: ${src}`));
     });
 }
@@ -44,8 +55,28 @@ function loadAsyncCSS(src, onLoad = null, parent = document.head) {
             if (typeof onLoad === 'function') onLoad(event);
             resolve(link)
         }, parent);
+
         link.onerror = () => reject(new Error(`Failed to load stylesheet: ${src}`));
     });
+}
+
+async function loadAsync(src, onLoad = null, parent = document.head) {
+    if (Array.isArray(src)) return Promise.all(src.map(s => loadAsync(s, onLoad, parent)));
+
+    try {
+        const response = await fetch(src);
+        const contentType = response.headers.get('content-type');
+
+        if (contentType.includes('text/javascript') || contentType.includes('application/javascript')) {
+            return loadAsyncJS(src, onLoad, parent);
+        } else if (contentType.includes('text/css')) {
+            return loadAsyncCSS(src, onLoad, parent);
+        }
+
+        throw new Error(`Unsupported content type: ${contentType}`)
+    } catch (error) {
+        throw new Error(`Failed to get content type: ${src}`);
+    }
 }
 
 async function loadAsyncJSWithError(src, onLoad = null, parent = document.head) {
@@ -61,6 +92,15 @@ async function loadAsyncCSSWithError(src, onLoad = null, parent = document.head)
     try {
         const link = await loadAsyncCSS(src, onLoad, parent);
         return [link, null];
+    } catch (error) {
+        return [null, error];
+    }
+}
+
+async function loadAsyncWithError(src, onLoad = null, parent = document.head) {
+    try {
+        const file = await loadAsync(src, onLoad, parent);
+        return [file, null];
     } catch (error) {
         return [null, error];
     }
